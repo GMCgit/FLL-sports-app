c9d1414707f8c1339f4daa7dff13cc3e7ca44b4c
GMCgit <goranivancic117@gmail.com>
2020-10-18 13:28:59 +0200

    Login and register form

---
+++ LoggingIn/login/login.js
@@ -0,0 +1,21 @@
+let db = firebase.firestore();
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+function LogIn() {
+  if (username.value == "" || password.value == "") {
+    alert("You didn't fill in all the fields");
+  } else {
+    db.collection("users")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          if (username.value == doc.data().username) {
+            if (password.value == doc.data().password) {
+              window.location = "../../index.html";
+            }
+          }
+        });
+      });
+  }
+}
---
+++ LoggingIn/register/register.js
@@ -0,0 +1,39 @@
+let db = firebase.firestore();
+const username = document.getElementById("userName");
+const password = document.getElementById("password");
+const name = document.getElementById("name");
+
+function register() {
+  if (username.value !== "" && password.value !== "" && name.value !== "") {
+    let takenUser = false;
+    db.collection("users")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          if (username.value == doc.data().username) {
+            takenUser = true;
+          }
+        });
+      });
+
+    if (takenUser == false) {
+      db.collection("users")
+        .add({
+          username: username.value,
+          password: password.value,
+          name: name.value,
+        })
+        .then((docRef) => {
+          console.log("Success!");
+          window.location.href = "../../index.html?name=Test";
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      alert("Username taken")
+    }
+  } else {
+    alert("You have to fill in all the fields");
+  }
+}
