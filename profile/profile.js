8ee98537a59547699542d792d4b3ba1eb1b32b2a
GMCgit <goranivancic117@gmail.com>
2020-10-19 09:39:38 +0200

    Added in some profile stuff

--- profile/profile.js
+++ profile/profile.js
@@ -2,6 +2,8 @@ let db = firebase.firestore();
 const username = document.getElementById("username");
 
 username.innerHTML = sessionStorage.getItem("username");
+document.getElementById("Username").innerHTML = `Username: ${sessionStorage.getItem("username")}`;
+document.getElementById("Name").innerHTML = `Name: ${sessionStorage.getItem("name")}`;
 
 function toMain() {
   window.location = `../mainPage/main.html`;
