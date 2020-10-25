84366037ee48e8ddf41ec924355c1c2b63bee33e
GMCgit <goranivancic117@gmail.com>
2020-10-25 19:01:03 +0100

    changes to display

--- othersProfile/others.js
+++ othersProfile/others.js
@@ -1,18 +1,25 @@
 let db = firebase.firestore();
 const username = document.getElementById("username");
 
-let otherObj = JSON.parse(sessionStorage.getItem("docId"))
+let otherObj = JSON.parse(sessionStorage.getItem("docId"));
 
-username.innerHTML = otherObj.username;
+username.innerHTML = sessionStorage.getItem("username");
+
+let profile = document.getElementById("profile");
+
+let otherUser = document.createElement("div");
+otherUser.classList.add("text");
+otherUser.innerHTML = `Username: ${otherObj.username}`;
+profile.appendChild(otherUser);
 
 function toMain() {
   window.location = `../mainPage/main.html`;
 }
 
 function toMap() {
-  window.location = `../map/map.html`
+  window.location = `../map/map.html`;
 }
 
 function toEditProfile() {
-  window.location = "../profile/editProfile/editProfile.html"
+  window.location = "../profile/editProfile/editProfile.html";
 }
