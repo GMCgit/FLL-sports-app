18521a2894bb1d7bb929750a62b65d8038b5fbcf
GMCgit <goranivancic117@gmail.com>
2020-12-10 12:15:13 +0100

    Leave button for in match at main

--- mainPage/main.js
+++ mainPage/main.js
@@ -19,6 +19,46 @@ function toAdmin() {
   window.location = "../adminPage/admin.html";
 }
 
+function leaveMatch() {
+  let stuff = JSON.parse(sessionStorage.getItem("fieldData"));
+  db.collection("fields")
+    .doc(stuff.field)
+    .get()
+    .then((doc) => {
+      if (stuff.difficulty == "easy") {
+        let index = doc.data().easy.indexOf(sessionStorage.getItem("DocName"));
+        let newArr = doc.data().easy;
+        newArr.splice(index, 1);
+        db.collection("fields").doc(stuff.field).update({
+          easy: newArr,
+        });
+      } else if (stuff.difficulty == "medium") {
+        let index = doc
+          .data()
+          .medium.indexOf(sessionStorage.getItem("DocName"));
+        db.collection("fields")
+          .doc(stuff.field)
+          .update({
+            medium: doc.data().medium.splice(index, 1),
+          });
+      } else if (stuff.difficulty == "hard") {
+        let index = doc.data().hard.indexOf(sessionStorage.getItem("DocName"));
+        db.collection("fields")
+          .doc(stuff.field)
+          .update({
+            hard: doc.data().hard.splice(index, 1),
+          });
+      }
+    }).then(() => {
+      sessionStorage.removeItem("fieldData");
+      document.getElementById("inMatch").classList.add("invis");
+      db.collection('users').doc(sessionStorage.getItem("DocName")).update({
+        inMatch: false,
+        fieldData: ""
+      })
+    });
+}
+
 db.collection("users")
   .doc(sessionStorage.getItem("DocName"))
   .get()
@@ -35,6 +75,8 @@ db.collection("users")
 
             document.getElementById("inMatchSport").innerHTML = stuff.sport;
             document.getElementById("inMatchName").innerHTML = stuff.name;
+
+            sessionStorage.setItem("fieldData", doc.data().fieldData);
           });
       } else {
         document.getElementById("inMatch").classList.remove("invis");
