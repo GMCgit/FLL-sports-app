fbba40ea7cf80190862397f676c32e5c34c3f5e7
GMCgit <goranivancic117@gmail.com>
2020-12-11 20:31:31 +0100

    Friends/blocked added

---
+++ Friends-Blocked/Friends-blocked.js
@@ -0,0 +1,24 @@
+let db = firebase.firestore();
+const username = document.getElementById("username");
+
+username.innerHTML = sessionStorage.getItem("username");
+
+if (sessionStorage.getItem("admin") == "true") {
+  document.getElementById("adminPage").classList.remove("invis");
+}
+
+function toProfile() {
+  window.location = `../profile/profile.html`;
+}
+
+function toMap() {
+  window.location = "../map/map.html";
+}
+
+function toAdmin() {
+  window.location = "../adminPage/admin.html";
+}
+
+function toMain() {
+  window.location = `../mainPage/main.html`;
+}
